0b42f92d49a6cb3dd9c0af6b8726d5d0bb84d70c
Your Name <you@example.com>
2021-02-22 09:32:00 +0000

    test opencv stitching: added dummy images

---
+++ project_misc/test_opencv_stitching/stitch.py
@@ -0,0 +1,10 @@
+from pyspark.sql import SparkSession
+
+
+
+
+if __name__ == '__main__':
+    spark = SparkSession\
+            .builder\
+            .appName("PythonSimpleStitch")\
+            .getOrCreate()
