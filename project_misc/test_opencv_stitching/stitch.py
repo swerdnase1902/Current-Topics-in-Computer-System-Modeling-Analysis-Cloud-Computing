fac88c9aa8632baad61add2746e0e8704047e58c
Your Name <you@example.com>
2021-02-23 07:51:30 +0000

    most basic stitching in pyspark

--- project_misc/test_opencv_stitching/stitch.py
+++ project_misc/test_opencv_stitching/stitch.py
@@ -1,22 +1,19 @@
 from pyspark.sql import SparkSession
 import numpy as np
 from PIL import Image
-import cv2 as cv
+import cv2
 import io
-
+import imutils
 
 def stitch(img_pair):
-    # https://stackoverflow.com/questions/60194755/opencv-read-images-from-pyspark-and-pass-to-a-keras-model
-    # name, img = binary_images
-    # pil_image = Image.open(io.BytesIO(img)).convert('RGB')
-    # cv2_image = numpy.array(pil_image)
-    # cv2_image = cv2_image[:, :, ::-1].copy()
-    _, left_img = img_pair[0]
-    left_img = np.array(Image.open(io.BytesIO(left_img)).convert('RGB'))[:, :, ::-1].copy()
-    _, right_img = img_pair[1]
-    right_img = np.array(Image.open(io.BytesIO(right_img)).convert('RGB'))[:, :, ::-1].copy()
+    stitcher = cv2.createStitcher() if imutils.is_cv3() else cv2.Stitcher_create()
+    ret = (status, stitched) = stitcher.stitch([img_pair[0], img_pair[1]])
+    return ret
 
-    return 'Hello'
+def binary_to_cv(binaryfile):
+    img = np.array(Image.open(io.BytesIO(binaryfile[1])).convert('RGB'))[:, :, ::-1].copy()
+    return img
+
 
 if __name__ == '__main__':
     spark = SparkSession \
@@ -25,8 +22,15 @@ if __name__ == '__main__':
         .getOrCreate()
     sc = spark.sparkContext
     left_images = sc.binaryFiles('example_img/left', minPartitions=sc.defaultMinPartitions)
+    left_images = left_images.map(lambda img: binary_to_cv(img))
     right_images = sc.binaryFiles('example_img/right', minPartitions=sc.defaultMinPartitions)
+    right_images = right_images.map(lambda img: binary_to_cv(img))
 
     pairs = left_images.zip(right_images)
     processed = pairs.map(lambda p: stitch(p))
+    processed = processed.collect()
+    for (status, img) in processed:
+        if status==0:
+            cv2.imshow('stitched image', img)
+            cv2.waitKey(0)
     exit(0)
